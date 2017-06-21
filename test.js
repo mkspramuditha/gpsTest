b44ae2ec94a6254c740cd4e7e68f2c8e99bb8cd5
mkspramuditha <mkspramuditha@gmail.com>
2017-06-21 23:14:41 +0530

    Solve token expiration

--- test.js
+++ test.js
@@ -1,17 +1,21 @@
 var http = require('http');
 var cron = require('cron');
-
-var jsonfile = require('jsonfile')
-
-var file = 'data.json'
+var request = require("request");
 
 
+var jsonfile = require('jsonfile');
+
+var file = 'data.json';
+
+var accessToken,refreshToken;
+
+getToken();
 var cronJob = cron.job("0 * * * * *", function(){
     get();
 });
 cronJob.start();
 
-var accessToken = "fcf8a1da88251556b064534daf8b5c24";
+// var accessToken = "fcf8a1da88251556b064534daf8b5c24";
 
 var interval = 10*1000;
 var dataArray = [];
@@ -20,7 +24,6 @@ var dataArray = [];
 
 function get() {
 
-    var request = require("request");
 
     var options = { method: 'GET',
         url: 'https://ideabiz.lk/apicall/iLocate/v1/getCurrentLocation/777336201',
@@ -34,7 +37,13 @@ function get() {
         if (error) throw new Error(error);
 
         console.log(body);
-        var details = JSON.parse(body)[0];
+
+        try{
+            var details = JSON.parse(body)[0];
+        }catch (e){
+            getToken();
+        }
+
         console.log(details.number);
         var latitude = details.lat;
         var longitude = details.lon;
@@ -54,4 +63,31 @@ function get() {
         })
     });
 
+}
+
+function getToken() {
+    var options = {
+        method: 'POST',
+        url: 'https://ideabiz.lk/apicall/token',
+        qs:
+            { grant_type: 'password',
+                scope: 'PRODUCTION',
+                username: 'EffectiveSolution',
+                password: 'Esol@123' },
+        headers:
+            {
+                'cache-control': 'no-cache',
+                'content-type': 'application/x-www-form-urlencoded',
+                authorization: 'Basic RFQ0dWFRRlFUUjQ0NURiOXY0UW1FQ1BwWUlJYTpBblBfVU1oY3ZqaXJZWENlanVJSTZJdGNHT3Nh'
+            }
+    };
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        var tokenResponse = JSON.parse(body);
+        refreshToken = tokenResponse.refresh_token;
+        accessToken = tokenResponse.access_token;
+        return true;
+    });
 }
